176388fcbb88647f7e1a1b7a35a10b5cd1ad3450
Stefan Pöschel <github@basicmaster.de>
2018-12-23 23:11:05 +0100

    Use set -e instead of exit for each cmd

--- simple_dab_live.sh
+++ simple_dab_live.sh
@@ -1,38 +1,41 @@
 #!/bin/bash -x
 
+# abort on error
+set -e
+
 # install packages
-sudo apt-get update || exit 1
-sudo apt-get -y install build-essential git cmake libmpg123-dev libfaad-dev libsdl2-dev libgtkmm-3.0-dev libfftw3-dev libsndfile1-dev libsamplerate0-dev librtlsdr-dev || exit 1
+sudo apt-get update
+sudo apt-get -y install build-essential git cmake libmpg123-dev libfaad-dev libsdl2-dev libgtkmm-3.0-dev libfftw3-dev libsndfile1-dev libsamplerate0-dev librtlsdr-dev
 
 # compile/install DABlin
-git clone -b next https://github.com/Opendigitalradio/dablin.git || exit 1
-pushd dablin || exit 1
+git clone -b next https://github.com/Opendigitalradio/dablin.git
+pushd dablin
 
-mkdir build || exit 1
-cd build || exit 1
-cmake .. || exit 1
-make -j 3 || exit 1
-sudo make install || exit 1
+mkdir build
+cd build
+cmake ..
+make -j 3
+sudo make install
 
-popd || exit 1
+popd
 
 # compile/install eti-cmdline
-git clone https://github.com/JvanKatwijk/eti-stuff.git || exit 1
-pushd eti-stuff || exit 1
+git clone https://github.com/JvanKatwijk/eti-stuff.git
+pushd eti-stuff
 
-cd eti-cmdline || exit 1
-mkdir build || exit 1
-cd build || exit 1
-cmake .. -DRTLSDR=ON || exit 1
-make -j 3 || exit 1
-sudo make install || exit 1
+cd eti-cmdline
+mkdir build
+cd build
+cmake .. -DRTLSDR=ON
+make -j 3
+sudo make install
 
-popd || exit 1
+popd
 
 # create call script
-mkdir -p ~/bin || exit 1
+mkdir -p ~/bin
 
-echo dablin_gtk -d eti-cmdline-rtlsdr -D eti-cmdline > ~/bin/dablin_calling || exit 1
-chmod +x ~/bin/dablin_calling || exit 1
+echo dablin_gtk -d eti-cmdline-rtlsdr -D eti-cmdline > ~/bin/dablin_calling
+chmod +x ~/bin/dablin_calling
 
 echo ----- Script successfully finished - just call "dablin_calling" after the next login! -----
